0e3264f0c665dbf60b88acf99080a12bff20a32a
SangHun <shzero211@naver.com>
2023-02-05 17:40:33 +0900

    feat #3: 스프링 시큐리티 도입 및 초기 설정 - 회원가입 URL permitAll - JWT 사용으로 불필요한 세션 생성방식 끄기 - 비밀번호 암호화를 위한 Encoder 빈 생성

---
+++ src/main/java/site/shkrr/kreamAuction/common/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package site.shkrr.kreamAuction.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf().disable()
+                .httpBasic().disable()
+                .authorizeRequests(x->
+                                 x.antMatchers("/user").permitAll()
+                                .anyRequest().authenticated())
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
