ed7873758bcf051fba431995ef695e0abc2d3ef4
SangHun <shzero211@naver.com>
2023-02-24 16:42:38 +0900

    feat #18: 주소 도메인 추가

---
+++ src/main/java/site/shkrr/kreamAuction/domain/address/AddressRepository.java
@@ -0,0 +1,6 @@
+package site.shkrr.kreamAuction.domain.address;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address,Long> {
+}
