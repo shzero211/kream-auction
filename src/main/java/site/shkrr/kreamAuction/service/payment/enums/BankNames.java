c9099f4b4f726a54726eb25847fdb8381305b93e
SangHun <shzero211@naver.com>
2023-03-08 16:10:00 +0900

    refactor #18: toss key 네이밍 변경, BankNames-> 은행명 추가, LocalRunner-> @profile 추가

--- src/main/java/site/shkrr/kreamAuction/service/payment/enums/BankNames.java
+++ src/main/java/site/shkrr/kreamAuction/service/payment/enums/BankNames.java
@@ -6,7 +6,14 @@ import lombok.Getter;
 * */
 @Getter
 public enum BankNames {
-    IBK_BC("3K"),GWANGJUBANK("46"),LOTTE("71"),WOORI("33");
+    IBK_BC("3K"),GWANGJUBANK("46"),LOTTE("71"),
+    KDBBANK("30"),BC("31"),SAMSUNG("51"),SAEMAUL("38"),
+    SHINHAN("41"),SHINHYEOP("62"),CITI("36"),
+    WOORI("33"),POST("37"),SAVINGBANK("39"),
+    JEONBUKBANK("35"),JEJUBANK("42"),KAKAOBANK("15"),
+    KBANK("3A"),HYUNDAI("61"),TOSSBANK("24"),
+    HANA("21"),KOOKMIN("11"),NONGHYEOP("91"),
+    SUHYEOP("34");
     private String bankCode;
     BankNames(String bankCode){
         this.bankCode=bankCode;
