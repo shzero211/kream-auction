41d7ae70fc8a99a22fa0c1fcce512f10573217bc
SangHun <shzero211@naver.com>
2023-03-25 18:25:14 +0900

    feat #22:  주석및 변경내용저장

--- src/main/java/site/shkrr/kreamAuction/controller/payment/PaymentController.java
+++ src/main/java/site/shkrr/kreamAuction/controller/payment/PaymentController.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.*;
 import site.shkrr.kreamAuction.common.utils.Utils;
 import site.shkrr.kreamAuction.controller.dto.PaymentDto;
 import site.shkrr.kreamAuction.controller.dto.PaymentDto.BillingRequest;
-import site.shkrr.kreamAuction.controller.dto.ProductDto.ProductInfo;
+import site.shkrr.kreamAuction.controller.dto.PaymentDto.BillingRequestCardInfo;
 import site.shkrr.kreamAuction.domain.user.User;
 import site.shkrr.kreamAuction.service.payment.PaymentService;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/payment")
@@ -22,7 +24,8 @@ public class PaymentController {
     private String clientKey;
 
     private final PaymentService paymentService;
-    //카드인증 클라이언트(빌링키와 함께사용될 고객키도 랜덤생성해서 전송)
+
+    //카드인증 클라이언트 접속(빌링키와 함께사용될 고객키도 랜덤생성해서 전송)
     @GetMapping("/client")
     public String openClient(Model model){
         String str=Utils.random.makeRandomKey();
@@ -31,18 +34,20 @@ public class PaymentController {
         return "payment/payment_client";
     }
 
+    //파라미터로 결제 수단 등록에 필요한 정보를 입력받아 결제수단 등록
     @GetMapping("")
     @ResponseBody
-    public ResponseEntity save(BillingRequest requestDto, @AuthenticationPrincipal User loginUser){
+    public ResponseEntity savePaymentForClient(BillingRequest requestDto, @AuthenticationPrincipal User loginUser){
         paymentService.save(requestDto,loginUser);
-        return Utils.response.of("빌링키 발급 성공");
+        return Utils.response.of("Client 로 결제 수단 등록");
     }
 
+    //카드정보로 결제 수단 등록
     @PostMapping("")
     @ResponseBody
-    public ResponseEntity payForPayment(@AuthenticationPrincipal User loginUser, @RequestBody ProductInfo requestDto){
-        paymentService.pay(loginUser,requestDto);
-        return Utils.response.of("결제 성공");
+    public ResponseEntity savePaymentForCard(@Valid @RequestBody BillingRequestCardInfo requestDto, @AuthenticationPrincipal User loginUser){
+        paymentService.saveByCard(requestDto,loginUser);
+        return Utils.response.of("카드 로 결제 수단 등록");
     }
 
     @PostMapping("/cancel")
